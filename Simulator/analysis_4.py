6953c1d16d15f2adc02d60425c4e429f2b7b4d81
Rafael <rafasampaio1@gmail.com>
2017-01-03 20:13:21 -0200

    Analysis4 calculate N_q1 and N_q2

--- Simulator/analysis_4.py
+++ Simulator/analysis_4.py
@@ -3,7 +3,7 @@ import math
 #class that receive states of a queue and calculate individualy usefull quantities
 #like /ro, E[U], E[X_r]...
 class CalculateParameters:
-    '''def __init__(self):
+    def __init__(self):
         #self.N=0
         self.N_q=0
         self.N_s=0
@@ -18,24 +18,44 @@ class CalculateParameters:
         self.allQueues=[]
         self.lastOut=-1
         self.times=[]
+        self.N_q1=0
+        self.N_q2=0
+        
+        self.W_1=0
+        self.W_2=0
+        self.T_1=0
+        self.T_2=0
+        self.X_1=0
+        self.X_2=0
+        self.X_r1=0
+        self.X_r2=0
 
     #function that receive a new state of queue and update the parameters
     def updateQueue(self,queue):
         #calculate N_q and N_s only if T differs from 0
         if self.counter!=0:
-            self.N_q+=len(queue[2])*(queue[0][0]-self.lastT)
-            if len(queue[3])>0:
+            self.N_q+=len(queue[3])*(queue[0][0]-self.lastT)
+            counter1=0
+            counter2=0
+            for q1 in queue[3]:
+                if q1[0]==1:
+                    counter1+=1
+                elif q1[0]==2:
+                    counter2+=1
+            self.N_q1+=counter1*(queue[0][0]-self.lastT)
+            self.N_q2+=counter2*(queue[0][0]-self.lastT)
+            if len(queue[4])>0:
                 self.N_s+=(queue[0][0]-self.lastT)
 
         #calculate T, X and W and X_r
-        if len(queue[3])>0 and queue[3][0]!=self.lastOut:
-            self.X_2+=(queue[3][2])**2
-            self.X+=queue[3][2]
-            self.T+=queue[3][3]-queue[3][1]
-            self.W+=queue[3][3]-queue[3][1]-queue[3][2]
-            self.times.append([queue[3][2],queue[3][3]-queue[3][1],queue[3][3]-queue[3][1]-queue[3][2],(queue[3][2])**2])
+        if len(queue[4])>0 and queue[4][0]!=self.lastOut:
+            self.X_2+=(queue[4][3])**2
+            self.X+=queue[4][3]
+            self.T+=queue[4][4]-queue[4][2]
+            self.W+=queue[4][4]-queue[4][2]-queue[4][3]
+            self.times.append([queue[4][3],queue[4][4]-queue[4][2],queue[4][4]-queue[4][2]-queue[4][3],(queue[4][3])**2])
 
-            self.lastOut=queue[3][0]
+            self.lastOut=queue[4][1]
             self.counterTime+=1
 
         self.counter+=1
@@ -53,7 +73,7 @@ class CalculateParameters:
         conf=0
         N_q=self.N_q/self.lastT
         for i in self.allQueues:
-            conf+=(len(i[2])-N_q)**2
+            conf+=(len(i[3])-N_q)**2
         conf/=(len(self.allQueues)-1)
         conf=1.96*conf/math.sqrt(len(self.allQueues))
         ret.append([N_q,conf])
@@ -62,7 +82,7 @@ class CalculateParameters:
         conf=0
         N_s=self.N_s/self.lastT
         for i in self.allQueues:
-            if(len(i[3])>0):
+            if(len(i[4])>0):
                 conf+=(1-N_s)**2
             else:
                 conf+=N_s**2
@@ -74,7 +94,7 @@ class CalculateParameters:
         conf=0
         N=(self.N_q+self.N_s)/self.lastT
         for i in self.allQueues:
-            if(len(i[3])>0):
+            if(len(i[4])>0):
                 conf+=(len(i[2])+1-N)**2
             else:
                 conf+=(len(i[2])-N)**2
@@ -118,13 +138,38 @@ class CalculateParameters:
         conf=1.96*conf/math.sqrt(len(self.times))
         ret.append([X_r,conf])
 
+        #N_q1 and interval calculation
+        conf=0
+        N_q1=self.N_q1/self.lastT
+        for i in self.allQueues:
+            counter=0
+            for j in i[3]:
+                if j[0]==1:
+                    counter+=1
+            conf+=(counter-N_q1)**2
+        conf/=(len(self.allQueues)-1)
+        conf=1.96*conf/math.sqrt(len(self.allQueues))
+        ret.append([N_q1,conf])
 
-        #return [ [N_q , Err] , [N_s , Err] , [N , Err] , [X , Err] , [T , Err] , [W , Err] , [X_r , err]]
+        #N_q2 and interval calculation
+        conf=0
+        N_q2=self.N_q2/self.lastT
+        for i in self.allQueues:
+            counter=0
+            for j in i[3]:
+                if j[0]==2:
+                    counter+=1
+            conf+=(counter-N_q2)**2
+        conf/=(len(self.allQueues)-1)
+        conf=1.96*conf/math.sqrt(len(self.allQueues))
+        ret.append([N_q2,conf])
+
+        #return [ [N_q , Err] , [N_s , Err] , [N , Err] , [X , Err] , [T , Err] , [W , Err] , [X_r , err] , [N_q1 , err] [N_q1 , err] ]
         return ret
 
     #function that  reinicialize the queue
     def reinitQueue(self):
-        self.__Init__()'''
+        self.__Init__()
 
 
 #MAIN
@@ -191,13 +236,13 @@ if __name__ == "__main__":
             queue.append(wait)
             queue.append(out)
             #queue pronto trabalhar a partir daqui
-            print(queue)
-            '''parameters.updateQueue(queue)'''
+            #print(queue)
+            parameters.updateQueue(queue)
             wait=[]
             queue=[]
 
     inputFile.close()
-    '''result=parameters.returnResults()
+    result=parameters.returnResults()
     print ('E[N]=%f +- %f\n' %(result[2][0] ,result[2][1]))
     print ('E[N_q]=%f +- %f\n' %(result[0][0] ,result[0][1]))
     print ('E[N_s]=%f +- %f\n' %(result[1][0] ,result[1][1]))
@@ -205,5 +250,7 @@ if __name__ == "__main__":
     print ('E[T]=%f +- %f\n' %(result[4][0] ,result[4][1]))
     print ('E[W]=%f +- %f\n' %(result[5][0] ,result[5][1]))
     print ('E[X_r]=%f +- %f\n' %(result[6][0] ,result[6][1]))
+    print ('E[N_q1]=%f +- %f\n' %(result[7][0] ,result[7][1]))
+    print ('E[N_q2]=%f +- %f\n' %(result[8][0] ,result[8][1]))
     print("\n")
-    print(result)'''
+    print(result)
